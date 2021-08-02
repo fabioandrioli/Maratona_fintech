7695f9face3e5fafa789822973f74facad0c0f0c
Fabio <fabio.drioli@gmail.com>
2021-08-02 15:58:15 -0300

    redução do código, troca do metodo open e close pelo metdodo toggle

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const Modal = {
+    toggle(){
+      //Abrir e fechar o modal
+      document
+              .querySelector('.modal-overlay')
+              .classList
+              .toggle('active');
+    },
+}
